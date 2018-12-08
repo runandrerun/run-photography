2fcd1daf203906d661b4e4525d7ecbb5d2c80466
runandrerun <the.asantiagojr@gmail.com>
2018-12-08 17:10:28 -0500

    Redux Implementation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+
+  // handleClick = () => {
+  //   console.log('hello')
+  //   this.props.changeExampleMessage()
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
         </header>
+        {/* <h3> { this.props.exampleMessage }</h3>
+        <button onClick={this.handleClick}> Click me </button> */}
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
+
+// const mapStateToProps = (state) => {
+//   return {
+//     exampleMessage: state.exampleState.exampleMessage
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     changeExampleMessage: () => { dispatch(changeExampleMessage()) }
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
