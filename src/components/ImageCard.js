7b971301f8bb2dea60b5d8ddeb4b42e6cb6216e8
runandrerun <the.asantiagojr@gmail.com>
2018-12-12 17:01:27 -0500

    Added React Components

---
+++ src/components/ImageCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  render() {
+    return(
+
+    )
+  }
+};
+
+export default ImageCard;
---
+++ src/containers/GalleryContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GalleryContainer extends Component {
+  render() {
+    return (
+
+    )
+  }
+};
+
+export default GalleryContainer;
