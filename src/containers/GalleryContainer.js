be3d32b8ac85739113b4ad82839337984452791c
runandrerun <the.asantiagojr@gmail.com>
2018-12-14 15:55:18 -0500

    Further additions

--- src/containers/GalleryContainer.js
+++ src/containers/GalleryContainer.js
@@ -12,3 +12,5 @@ export default GalleryContainer;
 
 
 // fetch('https://api.petfinder.com/breed.list?format=json&key=ef10db06ca9a615a109d001dbe6a2e53&animal=dog').then(res => res.json()).then(console.log)
+
+// fetch('http://api.petfinder.com/pet.getRandom?key=' + 'ef10db06ca9a615a109d001dbe6a2e53' + '&animal=cat&location=' + '10455' + '&output=basic&format=json&callback=?').then(res => res.json())
