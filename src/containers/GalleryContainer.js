a43807733747552e5053fafed07634d5c601b350
runandrerun <the.asantiagojr@gmail.com>
2018-12-13 20:42:05 -0500

    Tweaks

--- src/containers/GalleryContainer.js
+++ src/containers/GalleryContainer.js
@@ -9,3 +9,6 @@ class GalleryContainer extends Component {
 };
 
 export default GalleryContainer;
+
+
+// fetch('https://api.petfinder.com/breed.list?format=json&key=ef10db06ca9a615a109d001dbe6a2e53&animal=dog').then(res => res.json()).then(console.log)
