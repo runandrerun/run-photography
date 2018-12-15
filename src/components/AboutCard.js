d39a24368c777f33a30a313cb404974679a80781
runandrerun <the.asantiagojr@gmail.com>
2018-12-15 10:57:19 -0500

    Added About Me component

---
+++ src/components/AboutCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class AboutCard extends Component {
+  render() {
+    return (
+        <div className="about-me">
+          <div>
+            <p>This is Me!</p>
+          </div>
+        </div>
+    )
+  };
+};
