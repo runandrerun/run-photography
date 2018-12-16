a0e0282cd3bf24382e890804f7d071a2e3b42310
runandrerun <the.asantiagojr@gmail.com>
2018-12-16 07:57:21 -0500

    Contact Component

---
+++ src/components/ContactCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ContactCard extends Component {
+  render() {
+    return (
+      <div className="contact-info">
+        <p>Contact Details!</p>
+      </div>
+    )
+  };
+};
